b983f476e48f56b96d8a8955a4385daeecb82905
nodoid <paul@snuberry-software.co.uk>
2015-02-16 09:55:47 +0000

    Intiial import

---
+++ Lifecycle/Lifecycle/Activity2.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace Lifecycle
+{
+    [Activity(Label = "Activity2")]			
+    public class Activity2 : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Activity2);
+            Toast.MakeText(this, "OnCreate-Activity2", ToastLength.Short).Show();
+            var btn1 = FindViewById<Button>(Resource.Id.btnCAllActivity1);
+            btn1.Click += delegate
+            {
+                Finish();
+            };
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            Toast.MakeText(this, "OnStart-Activity2", ToastLength.Short).Show();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Toast.MakeText(this, "OnResume-Activity2", ToastLength.Short).Show();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            Toast.MakeText(this, "OnPause-Activity2", ToastLength.Short).Show();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            Toast.MakeText(this, "OnDestroy-Activity2", ToastLength.Short).Show();
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            Toast.MakeText(this, "OnStop-Activity2", ToastLength.Short).Show();
+        }
+    }
+}
+
---
+++ Lifecycle/Lifecycle/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Widget;
+using Android.OS;
+
+namespace Lifecycle
+{
+    [Activity(Label = "Lifecycle", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Activity1);
+            Toast.MakeText(this, "OnCreate-Activity 1", ToastLength.Short).Show();
+            var btn2 = FindViewById<Button>(Resource.Id.btnCAllActivity2);
+            btn2.Click += delegate
+            {
+                var intent = new Intent(this, typeof(Activity2));
+                intent.SetAction(Intent.ActionView);
+                StartActivity(intent);
+            };
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            Toast.MakeText(this, "OnStart-Activity1", ToastLength.Short).Show();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Toast.MakeText(this, "OnResume-Activity1", ToastLength.Short).Show();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            Toast.MakeText(this, "OnPause-Activity1", ToastLength.Short).Show();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            Toast.MakeText(this, "OnDestroy-Activity1", ToastLength.Short).Show();
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            Toast.MakeText(this, "OnStop-Activity1", ToastLength.Short).Show();
+        }
+    }
+}
+
+
